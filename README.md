3f22d4c21f8217cd903efb88f35228c1b4a473f3
Eugene Sharygin <eush77@gmail.com>
2015-07-14 23:36:21 +0300

    Fix API

--- README.md
+++ README.md
@@ -4,10 +4,33 @@
 
 [![Dependency Status][david-badge]][david]
 
-Convert package readme to a man page.
+Turn Markdown readme to a good-looking man page.
 
 [david]: https://david-dm.org/eush77/readme-to-man-page
 [david-badge]: https://david-dm.org/eush77/readme-to-man-page.png
+
+## Example
+
+```js
+var readme = fs.readFileSync('./README.md', { encoding: 'utf8' });
+var pkg = require('./package');
+
+readmeToManPage(readme, pkg)
+//=> '.TH "README-TO-MAN-PAGE" "npm" "July 2015" "0.1.2" "Nutty Programming Men"\n.SH "NAME"\n\\f...'
+```
+
+## API
+
+#### `readmeToManPage([readme], [opts])`
+
+`readme` defaults to `opts.readme`.
+
+Options:
+
+- `name`: package name.
+- `version`: package version.
+- `description`: package description.
+- `time`: readme date or string or object with `modified` field (as returned from the npm registry).
 
 ## Install
 
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-var registryUrl = require('registry-url')(),
-    got = require('got'),
-    npmExpansion = require('npm-expansion'),
+var npmExpansion = require('npm-expansion'),
+    assign = require('object.assign'),
     mdast = require('mdast'),
     mdastFile = require('mdast/lib/file'),
     mdastMan = require('mdast-man'),
@@ -12,53 +11,41 @@ var registryUrl = require('registry-url')(),
     mdAstToString = require('mdast-util-to-string');
 
 
-var manPage = function (pkg) {
+module.exports = function (readme, opts) {
+  if (typeof readme == 'object') {
+    opts = readme;
+    readme = opts.readme;
+  }
+
+  opts = opts || {};
+
+  if (opts.time && opts.time.modified) {
+    opts.time = opts.time.modified;
+  }
+
   var ast = mdast()
         .use(mdastStripBadges)
         .use(mdastSqueezeParagraphs)
-        .run(mdast.parse(pkg.readme));
-
-  var description = pkg.description;
+        .run(mdast.parse(readme));
 
   // Replace the default description with the content of the first
   // paragraph of readme body since they are often identical.
   mdAstVisit(ast, 'paragraph', function (node, index, parent) {
     var firstParagraph = mdAstToString(node);
-    var prefix = firstParagraph.slice(0, description.length);
-    if (prefix.toLowerCase() == description.toLowerCase()) {
-      description = firstParagraph;
+    var prefix = firstParagraph.slice(0, opts.description.length);
+    if (prefix.toLowerCase() == opts.description.toLowerCase()) {
+      opts.description = firstParagraph;
       parent.children.splice(index, 1);
     }
     return false;
   });
 
-  var manmd = mdast().use(mdastMan, {
+  var manmd = mdast().use(mdastMan, assign({}, opts, {
     section: 'npm',
-    manual: npmExpansion(),
-    name: pkg.name,
-    version: pkg.version,
-    description: description,
-    date: pkg.time.modified
-  });
+    manual: npmExpansion()
+  }));
 
   // mdast-man captures some settings on the File object.
   var file = mdastFile();
   return manmd.stringify(manmd.run(ast, file), file, {});
-};
-
-
-module.exports = function (name, opts, cb) {
-  if (typeof opts == 'function') {
-    cb = opts;
-    opts = {};
-  }
-
-  if (opts.man == null) {
-    opts.man = true;
-  }
-
-  got(registryUrl + name, { json: true }, function (err, pkg) {
-    if (err) return cb(err);
-    cb(err, opts.man ? manPage(pkg) : pkg.readme);
-  });
 };
