8385b736ba54f96147641d357b64f9ee5ba0a729
Eugene Sharygin <eush77@gmail.com>
2015-09-28 10:31:25 +0300

    chore: change require order

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 'use strict';
 
-var postprocess = require('./lib/postprocess'),
-    inferProps = require('./lib/infer-props'),
-    createDescriptionSection = require('./lib/create-description-section');
+var inferProps = require('./lib/infer-props'),
+    createDescriptionSection = require('./lib/create-description-section'),
+    postprocess = require('./lib/postprocess');
 
 var mdast = require('mdast'),
-    mdastMan = require('mdast-man'),
-    mdastStripBadges = require('mdast-strip-badges'),
-    mdastSqueezeParagraphs = require('mdast-squeeze-paragraphs'),
-    mdastNormalizeHeadings = require('mdast-normalize-headings'),
-    mdastToString = require('mdast-util-to-string');
+    stripBadges = require('mdast-strip-badges'),
+    squeezeParagraphs = require('mdast-squeeze-paragraphs'),
+    normalizeHeadings = require('mdast-normalize-headings'),
+    mdastMan = require('mdast-man');
 
 
 module.exports = function (readme, opts) {
   opts = opts || {};
 
   var manPage = mdast()
-        .use(mdastStripBadges)
-        .use(mdastSqueezeParagraphs)
-        .use(mdastNormalizeHeadings)
+        .use(stripBadges)
+        .use(squeezeParagraphs)
+        .use(normalizeHeadings)
         .use(inferProps, { props: opts })
         .use(createDescriptionSection, { skippedTypes: ['definition', 'html'] })
         .use(mdastMan, opts)
