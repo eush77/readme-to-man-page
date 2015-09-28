f80049395020ec2260091c1ba277865d466321f4
Eugene Sharygin <eush77@gmail.com>
2015-09-28 10:26:05 +0300

    chore: make a single pipeline

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var postprocess = require('./lib/postprocess');
+var postprocess = require('./lib/postprocess'),
+    inferProps = require('./lib/infer-props'),
+    createDescriptionSection = require('./lib/create-description-section');
 
 var mdast = require('mdast'),
-    vfile = require('vfile'),
     mdastMan = require('mdast-man'),
     mdastStripBadges = require('mdast-strip-badges'),
     mdastSqueezeParagraphs = require('mdast-squeeze-paragraphs'),
@@ -11,115 +12,17 @@ var mdast = require('mdast'),
     mdastToString = require('mdast-util-to-string');
 
 
-/**
- * Infer name from the top-level heading.
- */
-var inferName = function (ast) {
-  var inferredName;
-
-  ast.children.some(function (node) {
-    if (node.type == 'heading' && node.depth == 1) {
-      inferredName = mdastToString(node);
-      return true;
-    }
-  });
-
-  return inferredName;
-};
-
-
-/**
- * Find description in the first paragraph of the AST, return that and
- * remove corresponding nodes from the AST.
- *
- * Return the original description if not found.
- */
-var descriptionText = function (ast, description) {
-  ast.children.some(function (node, index) {
-    if (node.type == 'heading' && node.depth > 1) {
-      return true;
-    }
-
-    var text = mdastToString(node);
-    var prefix = text.slice(0, description.length);
-    if (prefix.toLowerCase() == description.toLowerCase()) {
-      description = text;
-      ast.children.splice(index, 1);
-      return true;
-    }
-  });
-
-  return description;
-};
-
-
-/**
- * Move anything other than title string away from the NAME section.
- */
-var createDescriptionSection = function (ast, skippedTypes) {
-  var startIndex;
-
-  if (!ast.children.length) {
-    return;
-  }
-  if (ast.children[0].type != 'heading') {
-    startIndex = 0;
-  }
-  else if (ast.children[0].depth == 1 && ast.children.length >= 2) {
-    startIndex = 1;
-    var startNode = ast.children[startIndex];
-
-    while (startNode && skippedTypes.indexOf(startNode.type) >= 0) {
-      startNode = ast.children[++startIndex];
-    }
-
-    if (!startNode || startNode.type == 'heading') {
-      return;
-    }
-  }
-  else {
-    return;
-  }
-
-  ast.children.splice(startIndex, 0, {
-    type: 'heading',
-    depth: 2,
-    children: [{
-      type: 'text',
-      value: 'DESCRIPTION'
-    }]
-  });
-};
-
-
 module.exports = function (readme, opts) {
   opts = opts || {};
 
-  var ast = mdast()
+  var manPage = mdast()
         .use(mdastStripBadges)
         .use(mdastSqueezeParagraphs)
         .use(mdastNormalizeHeadings)
-        .run(mdast.parse(readme, { position: false }));
+        .use(inferProps, { props: opts })
+        .use(createDescriptionSection, { skippedTypes: ['definition', 'html'] })
+        .use(mdastMan, opts)
+        .process(readme);
 
-  // Infer name from headings.
-  if (!opts.name) {
-    opts.name = inferName(ast);
-  }
-
-  // Try to replace the default description with the content of the first
-  // paragraph of readme body since they are often identical.
-  if (opts.description) {
-    opts.description = descriptionText(ast, opts.description);
-  }
-
-  // Create DESCRIPTION section if needed.
-  // Skip definitions and HTML nodes.
-  createDescriptionSection(ast, ['definition', 'html']);
-
-  var manmd = mdast().use(mdastMan, opts);
-
-  // mdast-man captures some settings on the File object.
-  var file = vfile();
-  var man = manmd.stringify(manmd.run(ast, file), file, {});
-  return postprocess(man, opts);
+  return postprocess(manPage, opts);
 };
